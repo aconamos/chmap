ce01f4eec936b016600828f9dc0b0907d59c869a
nephi@blaviken-wsl <nephi.norton@gmail.com>
2025-09-02 23:47:56 -0600

    had a change of heart about pointers; documentation

--- src/chmap.c
+++ src/chmap.c
@@ -12,7 +12,7 @@
 #define MAX_LOAD_FACTOR 0.9f
 
 
-static const char* SIPHASH_KEY = "abcdef9876543210";
+static const char * SIPHASH_KEY = "abcdef9876543210";
 
 struct __psl_ind { 
     uint64_t index; 
@@ -23,8 +23,8 @@ struct __psl_ind {
  * Takes an entry and a location and tries to insert it at the location, performing
  * robinhood shuffling if necessary to maintain low PSL or whatever.
  */
-void
-__bubble_up(struct chmap *map, const struct __entry inentry, size_t ind) {
+static void
+bubble_up(struct chmap * map, const struct __entry inentry, size_t ind) {
     struct __entry buffer_entry;
     struct __entry grabbed_entry = inentry;
 
@@ -54,8 +54,8 @@ __bubble_up(struct chmap *map, const struct __entry inentry, size_t ind) {
 /**
  * Find the next spot for a key
  */
-struct __psl_ind
-__find_spot(struct chmap *map, const uint64_t key, size_t psl, uint64_t working_index) {
+static struct __psl_ind
+find_spot(struct chmap * map, const uint64_t key, size_t psl, uint64_t working_index) {
     struct __entry working_entry = map->__translation_array[working_index];
 
     while (working_entry.has_entry == 1 && working_entry.keyword != key && working_entry.psl >= psl) {
@@ -70,20 +70,20 @@ __find_spot(struct chmap *map, const uint64_t key, size_t psl, uint64_t working_
 /**
  * Given a chmap and a key, returns the index where that key should be inserted and the PSL.
  */
-struct __psl_ind
-__address_array(struct chmap *map, const uint64_t key) 
+static struct __psl_ind
+address_array(struct chmap *map, const uint64_t key) 
 {
     uint64_t working_index = key % map->__array_size;
     size_t psl = 0;
 
-    return __find_spot(map, key, psl, working_index);
+    return find_spot(map, key, psl, working_index);
 }
 
-struct __entry*
-__init_translation_array(
+static const struct __entry *
+init_translation_array(
     const size_t numentries
 ) {
-    struct __entry *entries = calloc(numentries, sizeof(struct __entry));
+    struct __entry * entries = calloc(numentries, sizeof(struct __entry));
     struct __entry empty = {
         .has_entry = 0,
     };
@@ -96,18 +96,18 @@ __init_translation_array(
 }
 
 
-struct chmap*
+const struct chmap *
 chmap_new(
     const size_t item_size
 ) {
-    void *backing_array = malloc(sizeof(item_size) * DEFAULT_BACKING_ARRAY_LENGTH);
-    struct chmap *map = malloc(sizeof(struct chmap));
+    void * backing_array = malloc(sizeof(item_size) * DEFAULT_BACKING_ARRAY_LENGTH);
+    struct chmap * map = malloc(sizeof(struct chmap));
 
     map->__key_size = 0;
     map->__item_size = item_size;
     map->__used_size = 0;
     map->__array_size = DEFAULT_BACKING_ARRAY_LENGTH;
-    map->__translation_array = __init_translation_array(DEFAULT_BACKING_ARRAY_LENGTH);
+    map->__translation_array = init_translation_array(DEFAULT_BACKING_ARRAY_LENGTH);
     map->__backing_array = backing_array;
 
     return map;
@@ -115,10 +115,10 @@ chmap_new(
 
 int
 chmap_put(
-    struct chmap *map,
-    const void *key,
+    struct chmap * map,
+    const void * key,
     const size_t keysize,
-    const void *item,
+    const void * item,
     const size_t itemsize
 ) {
     // This is used in place of a uint8_t[8] to provide the same 8 bytes
@@ -129,7 +129,7 @@ chmap_put(
 
     siphash(key, keysize, SIPHASH_KEY, (uint8_t*)&outword, 8);
 
-    struct __psl_ind insert_at = __address_array(map, outword);
+    struct __psl_ind insert_at = address_array(map, outword);
     const struct __entry looking_at = map->__translation_array[insert_at.index];
 
     if (looking_at.has_entry == 0) {
@@ -146,12 +146,12 @@ chmap_put(
         
         map->__translation_array[insert_at.index] = new_entry;
 
-        void *ba_ptr = (size_t*)map->__backing_array + bak * itemsize;
+        void * ba_ptr = (size_t*)map->__backing_array + bak * itemsize;
 
         memcpy(ba_ptr, item, itemsize);
     } else if (looking_at.keyword == outword) {
         // This key already is associated - overwrite it
-        void *ba_ptr = (size_t*)map->__backing_array + looking_at.backing_array_key * itemsize;
+        void * ba_ptr = (size_t*)map->__backing_array + looking_at.backing_array_key * itemsize;
 
         memcpy(ba_ptr, item, itemsize);
     } else {
@@ -165,20 +165,20 @@ chmap_put(
             .keyword = outword,
         };
 
-        void *ba_ptr = (size_t*)map->__backing_array + bak * itemsize;
+        void * ba_ptr = (size_t*)map->__backing_array + bak * itemsize;
 
         memcpy(ba_ptr, item, itemsize);
 
-        __bubble_up(map, new_entry, insert_at.index);
+        bubble_up(map, new_entry, insert_at.index);
     }
 
     return 0;
 }
 
-void*
+const void *
 chmap_get(
-    struct chmap *map,
-    const void *key,
+    struct chmap * map,
+    const void * key,
     const size_t keysize
 ) {
     uint64_t outword;
@@ -202,7 +202,7 @@ chmap_get(
 
 void
 debug_map(
-    struct chmap *map
+    struct chmap * map
 ) {
     for (size_t i = 0; i < map->__array_size; i++) {
         struct __entry entry = map->__translation_array[i];
--- src/chmap.h
+++ src/chmap.h
@@ -16,13 +16,24 @@ struct __entry {
 struct chmap {
     // TODO: key_size might be pointless
     size_t __key_size;
+
+    // The size of any given item. This is needed for backing array allocation and indexing.
     size_t __item_size;
+
     // The number of elements stored in this map. Used to compute load factor, 
     // and for keeping track of the next index in the backing array
     size_t __used_size;
+
+    // The number of elements in the translation array and backing array.
     size_t __array_size;
-    struct __entry *__translation_array;
-    void *__backing_array;
+
+    // In more common vernacular, this is the array of buckets.
+    // It is where hashes key into and holds indices that reference
+    // a spot in the backing array to store data.
+    struct __entry * __translation_array;
+
+    // This is the array that holds actual data.
+    void * __backing_array;
 };
 
 
@@ -31,24 +42,23 @@ struct chmap {
  * 
  * @param item_size size of data that will be stored in this hashmap
  */
-struct chmap* chmap_new(const size_t item_size);
+const struct chmap * chmap_new(const size_t item_size);
 
 /**
  * Puts an item into the given map at the given key. Returns 1 if an item was overwritten,
  * or a 0 if the slot was empty.
  */
 int chmap_put(
-    struct chmap *map, 
-    const void *key, 
+    struct chmap * map, 
+    const void * key, 
     const size_t keysize, 
-    const void *item, 
+    const void * item, 
     const size_t itemsize
 );
 
 /**
  * Gets a pointer to the item associated with `key`, or `NULL` if not found.
  */
-void* chmap_get(struct chmap *map, const void *key, const size_t keysize);
+const void * chmap_get(struct chmap * map, const void * key, const size_t keysize);
 
-void
-debug_map(struct chmap *map);
+void debug_map(struct chmap * map);
